772efddd2527f00d3fe091d1b1b7a2f298e90fa9
perfumellim <perfumelim@gmail.com>
2020-11-02 21:41:00 +0900

    Add: Quiz 기능  구현

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -1,14 +1,91 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { KM_URL } from "../../config";
 import QuizComponents from "./QuizComponents";
+import "./Quiz.scss";
 
 class Quiz extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quiz_data: [],
+      answers: [],
+      answerIdx: 1,
+    };
+  }
+
+  handleAnswer = (title) => {
+    this.setState(
+      {
+        answers: [...this.state.answers, title],
+        answerIdx: this.state.answerIdx + 1,
+      },
+      () => {
+        if (this.state.answerIdx === 5) {
+          fetch(`${KM_URL}/products/recommend`, {
+            method: "POST",
+            body: JSON.stringify(this.state),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((res) => res.json())
+            .then((res) =>
+              this.props.history.push(`/products/${res.foundProductID}`)
+            );
+        } else this.props.history.push(`/${this.state.answerIdx}`);
+      }
+    );
+  };
+
+  componentDidMount() {
+    const { quizNums } = this.props.match.params;
+    fetch(`http://localhost:3000/Data/Quizdata.json`)
+      .then((Quizdata) => Quizdata.json())
+      .then((Quizdata) =>
+        this.setState({
+          quiz_data: Quizdata.quiz_data.filter(
+            (el) => el.id === Number(quizNums)
+          ),
+        })
+      );
+  }
+
+  componentDidUpdate(prevProps) {
+    const { quizNums } = this.props.match.params;
+    const prevId = prevProps.match.params.quizNums;
+    if (quizNums !== prevId) {
+      fetch(`http://localhost:3000/Data/Quizdata.json`)
+        .then((Quizdata) => Quizdata.json())
+        .then((Quizdata) => {
+          this.setState({
+            quiz_data: Quizdata.quiz_data.filter(
+              (el) => el.id === Number(quizNums)
+            ),
+          });
+        });
+    }
+  }
+
   render() {
+    const { quiz_data } = this.state;
     return (
-      <div>
-        <h1>test</h1>
-        <QuizComponents />
+      <div className="quizContents">
+        {quiz_data.map((quiz_data) => {
+          return (
+            <QuizComponents
+              handleAnswer={this.handleAnswer}
+              key={quiz_data.id}
+              id={quiz_data.id}
+              title={quiz_data.title}
+              img={quiz_data.img}
+              cards={quiz_data.cards}
+              contents={quiz_data.contents}
+            />
+          );
+        })}
       </div>
     );
   }
 }
-export default Quiz;
+export default withRouter(Quiz);
--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Quiz from "./Pages/Quiz/Quiz"; 추후 작업 예정
+import Quiz from "./Pages/Quiz/Quiz";
 import Login from "./Pages/Login/Login";
 import ProductList from "./Pages/ProductList/ProductList";
 import ProductDetails from "./Pages/ProductDetails/ProductDetails";
-import Navbar from "../src/Components/Navbar/Navbar";
-import Account from "../src/Pages/Account/Account";
+import Navbar from "./Components/Navbar/Navbar";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <Navbar />
         <Switch>
-          {/* <Route exact path="/" component={Quiz} /> */}
           <Route exact path="/Login" component={Login} />
           <Route exact path="/ProductList" component={ProductList} />
           <Route exact path="/ProductDetails/:id" component={ProductDetails} />
+          <Route exact path="/:quizNums" component={Quiz} />
         </Switch>
       </Router>
     );
