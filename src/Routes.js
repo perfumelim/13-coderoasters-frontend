85bae46e840f894407ba347aa538c572e8147b48
Jo Hye Mi <dev.summerr@gmail.com>
2020-11-03 22:15:23 +0900

    Add : google 소셜로그인 기능 구현

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import Login from "./Pages/Login/Login";
+import LoginButton from "./Pages/Login/LoginButton";
+// import MyMatches from "./Pages/MyMatches/MyMatches";
+// import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+// import ProductList from "./Pages/ProductList/ProductList";
 import Quiz from "./Pages/Quiz/Quiz";
 
 class Routes extends React.Component {
@@ -10,6 +15,12 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/" component={Quiz} />
           <Route exact path="/Navbar" component={Navbar} />
+          <Route exact path="/Login/Button" component={LoginButton} />
+          <Route exact path="/Login" component={Login} />
+
+          {/* <Route exact path="/MyMatches" component={MyMatches} />
+          <Route exact path="/ProductDetails" component={ProductDetails} />
+          <Route exact path="/ProductList" component={ProductList} /> */}
         </Switch>
       </Router>
     );
