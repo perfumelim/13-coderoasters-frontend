8a067726028db10a8e37be884441fea45f0ba9ff
김보현 <bohyunkim01@gmail.com>
2020-11-11 21:39:47 +0900

    Fix: Routes 파일 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Quiz from "./Pages/Quiz/Quiz";
-import Login from "./Pages/Login/Login";
+// import Quiz from "./Pages/Quiz/Quiz"; 추후 작업 예정
+// import Login from "./Pages/Login/Login"; 추후 작업 예정
 import ProductDetails from "./Pages/ProductDetails/ProductDetails";
 import ProductList from "./Pages/ProductList/ProductList";
 
@@ -10,8 +10,8 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Quiz} />
-          <Route exact path="/Login" component={Login} />
+          {/* <Route exact path="/" component={Quiz} />
+          <Route exact path="/Login" component={Login} /> */}
           <Route exact path="/ProductList" component={ProductList} />
           <Route exact path="/ProductDetails/:id" component={ProductDetails} />
         </Switch>
